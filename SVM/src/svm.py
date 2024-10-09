d3072096170317c917062a40431330f479e25255
Developer-WasimAkram <noorewasim@gmail.com>
2024-10-09 18:42:32 +0530

    svm classification

---
+++ SVM/src/svm.py
@@ -0,0 +1,34 @@
+import numpy as np  
+
+class SVM:
+    
+    def __init__(self, learning_rate=0.001, lambda_param=0.01, n_iters=1000):
+        self.lr = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iters = n_iters
+        self.w = None
+        self.b = None
+        
+        
+    def fit(self, X, y):
+        n_samples, n_features=X.shape 
+        
+        # Initialize parameters
+        self.w = np.zeros(n_features)
+        self.b = 0
+        
+        # Convert y to a binary class matrix
+        y_binarized = np.where(y <= 0, -1, 1)
+        
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                #condition check  y_idx(x_i*w-b)
+                if y_binarized[idx] * (np.dot(x_i, self.w) - self.b) < 1:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w - np.dot(x_i, y_binarized[idx]))
+                    self.b -= self.lr * y_binarized[idx]
+                else:
+                    self.w -= self.lr * 2 * self.lambda_param * self.w
+                    
+    def predict(self, X):
+        linear_output = np.dot(X, self.w) - self.b
+        return np.sign(linear_output)
